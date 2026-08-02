f3fcdd91598cab4c90af4a63f18a3b3482a55472
Ary Naim <naimaryan1@yahoo.com>
2026-08-02 13:06:01 -0700

    PyTorch 2.11 compatibility issue

--- train.py
+++ train.py
@@ -208,7 +208,7 @@ class Yolo_loss(nn.Module):
             truth_box[:n, 0] = truth_x_all[b, :n]
             truth_box[:n, 1] = truth_y_all[b, :n]
 
-            pred_ious = bboxes_iou(pred[b].view(-1, 4), truth_box, xyxy=False)
+            pred_ious = bboxes_iou(pred[b].reshape(-1, 4), truth_box, xyxy=False)
             pred_best_iou, _ = pred_ious.max(dim=1)
             pred_best_iou = (pred_best_iou > self.ignore_thre)
             pred_best_iou = pred_best_iou.view(pred[b].shape[:3])
