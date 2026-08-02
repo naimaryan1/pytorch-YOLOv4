95eb6c29591e0c4ee025109c590d99238c396993
Ary Naim <naimaryan1@yahoo.com>
2026-08-02 13:02:32 -0700

    updated tio handle no class case with no bounding box

--- dataset.py
+++ dataset.py
@@ -382,6 +382,10 @@ class Yolo_dataset(Dataset):
                 # print(img_path)
         if use_mixup == 3:
             out_bboxes = np.concatenate(out_bboxes, axis=0)
+            out_bboxes = np.asarray(
+            out_bboxes,
+            dtype=np.float32
+        ).reshape(-1, 5)
         out_bboxes1 = np.zeros([self.cfg.boxes, 5])
         out_bboxes1[:min(out_bboxes.shape[0], self.cfg.boxes)] = out_bboxes[:min(out_bboxes.shape[0], self.cfg.boxes)]
         return out_img, out_bboxes1
@@ -428,3 +432,4 @@ if __name__ == "__main__":
         a = draw_box(out_img.copy(), out_bboxes.astype(np.int32))
         plt.imshow(a.astype(np.int32))
         plt.show()
+
