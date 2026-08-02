1d95e29ee872738ced5a85395d851b4d7bff3ab9
Ary Naim <naimaryan1@yahoo.com>
2026-08-02 12:55:58 -0700

    trai.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 %%bash
-cd /content/pytorch-YOLOv4 || exit 1
+cd /content/gdrive/MyDrive/pytorch-YOLOv4/ || exit 1
 
 PATH_BACKBONE_WEIGHTS="/content/gdrive/MyDrive/pytorch-YOLOv4/yolov4.conv.137.pth"
 PATH_D_FIRE_DATA_ROOT="/content/gdrive/MyDrive/Smoke_and_Fire_Datasets/D-Fire"
@@ -12,3 +12,4 @@ python3 train.py \
   -classes 2 \
   -dir "$PATH_D_FIRE_DATA_ROOT" \
   -train_label_path "$PATH_TO_TRAIN_DATA_LIST"
+
